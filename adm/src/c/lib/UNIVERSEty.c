286c269525a1a3acb25435c00ad613d9e8bd22e6
alita <shyluckya10c81@gmail.com>
2023-09-04 14:23:02 -0400

    algorithm

---
+++ adm/src/c/lib/UNIVERSEty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    /*
+     *  place{ room1[studants[], teacher, knowledge], room2[], room3[], room4[]}
+     *  *rooms[*int *int *int]
+     *  *students[]
+     *  *teachers[]
+     *  *knowledges[]
+     * */
+
+}

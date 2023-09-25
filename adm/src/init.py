2afd2c770ea96ef207214e669af2dd746e07abb2
jubermiranda <shyluckya10c81@gmail.com>
2023-09-25 13:06:06 -0400

    tryin load files from txt files

--- adm/src/init.py
+++ adm/src/init.py
@@ -5,8 +5,35 @@ import datetime
 
 UNIVERSEty = ctypes.CDLL('./c_lib/shared/UNIVERSIty.so')
 
+rooms, timelines,  teachers, classes = []
+matriz_data = []
+file_name = "data.txt"
+
+
+def load_file():
+    try:
+        with open(file_name) as file_data
+        for line in file_data:
+            parts = line.strip().split(',')
+            if len(parts) == 4:
+                matriz_data.append(parts)
+            else:
+                print("(should be 4 elements comma separated):", line.strip())
+
+
+    except FileNotFoundError:
+        print('File ' + file_name + 'does not exist.')
+    except Exception as e:
+        print(e)
+
+
 def main():
-    file_ok = UNIVERSEty.check_file_data()
+    load_file()
+    if len(matriz_data) == 0:
+        print("no data loaded from file. wanna insert manually the data?")
+
+    
+            
 
 
 if __name__ == "__main__":

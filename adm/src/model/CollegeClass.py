4a0d31fd03cb46b03aea1826e9f04c13cbe1d7a5
jubermiranda <-->
2023-10-02 06:41:30 -0400

    dataclass add

---
+++ adm/src/model/CollegeClass.py
@@ -0,0 +1,10 @@
+class CollegeClass:
+    _name = ""
+    _lvl = 0
+    _workload = None
+
+    def __init__(self, name, lvl, workload):
+        _name = name
+        _lvl = lvl
+        _workload = workload
+        

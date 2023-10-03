a6752a7f9d11d9e230758dfa9061f3aaa3a30060
jubermiranda <-->
2023-10-03 03:28:15 -0400

    changing import file format

--- adm/src/init.py
+++ adm/src/init.py
@@ -2,27 +2,33 @@
 
 import ctypes
 import datetime
+from model.CollegeClass import CollegeClass
 
 
 UNIVERSEty = ctypes.CDLL('./c_lib/shared/UNIVERSIty.so')
 
 rooms, timelines,  teachers, college_classes = [], [], [], []
-#matriz_data = []
+matriz_data = []
 file_name = "data.txt"
 
 
 def load_file():
     try:
-        with open(file_name) as file_data
-        for line in file_data:
-            parts = line.strip().split(',')
-            if len(parts) == 4:
-                college_classes.append(CollegeClass(parts[0:3]))
-                teachers.append(parts[1])
-                teachers.append(parts[2])
-                teachers.append(parts[3])
-            else:
-                print("(should be 4 elements comma separated):", line.strip())
+        with open(file_name) as file_data:
+            len_insert = 0
+            for line in file_data:
+                parts = line.strip().split(',')
+                if len(parts) == 4:
+                    college_classes.append(CollegeClass(parts[0:4]))
+                    teachers.append(parts[1])
+                    teachers.append(parts[2])
+                    teachers.append(parts[3])
+                    len_insert+=1
+                else:
+                    print("(should be 4 elements comma separated):", line.strip())
+                    raise Exception("file format exception")
+
+            print(f"{len_insert} itens loaded!")
 
 
     except FileNotFoundError:
@@ -36,12 +42,10 @@ def main():
     if len(matriz_data) == 0:
         print("no data loaded from file. wanna insert manually the data?")
         return
+    else:
+        print("load itens from file")
 
     #for line in matriz_data:
-
-
-    
-            
 
 
 if __name__ == "__main__":
--- adm/src/model/CollegeClass.py
+++ adm/src/model/CollegeClass.py
@@ -1,10 +1,6 @@
 class CollegeClass:
-    _name = ""
-    _lvl = 0
-    _workload = None
-
     def __init__(self, name, lvl, workload):
-        _name = name
-        _lvl = lvl
-        _workload = workload
+        self.name = name
+        self.lvl = lvl
+        self.workload = workload
         
